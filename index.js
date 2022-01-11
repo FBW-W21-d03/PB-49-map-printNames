6a76cd93795ffef177971255780d964d6e584a29
slavablack <veaceslav.ciornii@magenta.de>
2022-01-11 09:48:55 +0100

    map

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ function firstName(names){
 console.log(names)
 }
 employees.map (firstName);
+
+//forEach
+const employees2=["John", "Chuck", "Henry", "Oscar", "Ryan", "Bob", "Pete"];
+employees2.forEach (
+    (namesEmploy) => {
+console.log(namesEmploy + " IT")
+
+    }
+)
+//
