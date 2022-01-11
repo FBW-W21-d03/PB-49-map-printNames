673f56eb36fde7c7cb833fbef955f4b78cf91749
sham912411 <shamalhakim91@gmail.com>
2022-01-11 11:50:47 +0100

    l

---
+++ script.js
@@ -0,0 +1,14 @@
+// 1.)
+
+const names = ["John", "Chuck", "Henry", "Oscar", "Ryan", "Bob", "Pete"];
+
+// 2.)
+
+function writeInConsole(firstName) {
+  console.log("Vorname:", firstName);
+}
+
+// 3.)
+
+names.map(writeInConsole);
+
