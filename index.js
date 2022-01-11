bee446668e62df2d4b172b9bdc89b69c91297a6c
Mohal1125 <rajwindersinghmohal@yahoo.com>
2022-01-11 09:44:41 +0100

    fertig

---
+++ index.js
@@ -0,0 +1,19 @@
+const namen = [
+    "John",
+    "Chuck",
+    "Henry",
+    "Oscar",
+    "Ryan",
+    "Bob",
+    "Pete"
+];
+
+/*namen.map ((firstname) => {
+   console.log(firstname);
+}
+);*/
+
+function firstName(item) {
+  console.log(item);
+}
+namen.map(firstName);
