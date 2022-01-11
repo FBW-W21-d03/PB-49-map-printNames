127c9415419159369efcf027eae5ed00652051ce
OrhanKadirov <orhan.kadirov.gs@gmail.com>
2022-01-11 09:56:54 +0100

    Orhan's lösung

---
+++ javascript-map-printNames.js
@@ -0,0 +1,15 @@
+const arrNamens = ["John", "Chuck", "Henry", "Oscar", "Ryan", "Bob", "Pete"];
+
+console.log("Aufgabe 1: \n", arrNamens);
+
+
+function firstName(firstName){
+    console.log(firstName); 
+}
+
+console.log("Aufgabe 2: \n", firstName);
+
+console.log("Aufgabe 3: \n");
+arrNamens.map(firstName);
+
+
