18ba6233d6cf17a07510422f6abd35f85576a2be
Sharfuddin Ahmed <Sharfuddinahmed07@gmail.com>
2022-01-11 10:04:51 +0100

    meine Lösung

---
+++ index.js
@@ -0,0 +1,10 @@
+function listOfNames() {
+    let firstname = [ "John", "Chuck", "Henry", "Oscar", "Ryan", "Bob", "Pete"]
+    
+    firstname.map((firstname) => {
+      console.log(firstname)
+    })
+  }
+  
+  listOfNames()
+  
