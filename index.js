29f4cac9d01246e0c0c2ad29f8306db690901197
sprotte <metatron1972@live.de>
2022-01-11 09:52:56 +0100

    sprotte

---
+++ index.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const namen = ["John", "Chuck", "Henry", "Oscar", "Ryan", "Bob", "Pete"];
+
+let array = [];
+
+array.map(myFunction);
+{
+  namen;
+}
+
+function myFunction(namen) {
+  console.log(namen);
+}
