c2182bc077a54da32afaeb53865b2fb4c486bdc7
kawanaza <kawanbabani@gmail.com>
2022-01-11 09:43:16 +0100

    Done mit erste aufgabe in .map

---
+++ index.js
@@ -0,0 +1,7 @@
+let namen = ["John", "Chuck", "Henry", "Oscar", "Ryan", "Bob", "Pete"];
+
+function myFunction(firstName) {
+  console.log(firstName);
+}
+namen.map(myFunction);
+console.log("First Name :",namen);
