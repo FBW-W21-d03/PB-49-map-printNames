f620ce0df1dff3526df3196faac6739c354fe437
H94ssein44 <alali.52@yahoo.com>
2022-01-11 10:01:37 +0100

    d

---
+++ index.js
@@ -0,0 +1,10 @@
+const namen =["John", "Chuck", "Henry", "Oscar", "Ryan", "Bob", "Pete" ];
+
+/* const namen = []; */
+
+function myFunction(firstName){
+    
+console.log(firstName);
+}
+namen.map(myFunction);
+
