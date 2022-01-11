d9e131e12735362402fea292b8e8329852dfed7d
kawanaza <kawanbabani@gmail.com>
2022-01-11 09:54:09 +0100

    Done

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 let namen = ["John", "Chuck", "Henry", "Oscar", "Ryan", "Bob", "Pete"];
-
+let neueNamen = [];
 function myFunction(firstName) {
+    neueNamen.push(firstName);
   console.log(firstName);
 }
 namen.map(myFunction);
 console.log("First Name :",namen);
+
+/// add new person for test///
+myFunction ("lukas");
+myFunction ("kawan");
+myFunction ("Julia");
